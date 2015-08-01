c52504b19893784cf1d3e8223f9527719e6a3fd7
Doddophonique <davide.oddone.21@gmail.com>
2015-08-01 11:26:55 +0200

    Cleaned Cross_MThread and added comments

--- app/src/main/java/classify/prove/doddo/libsvm_classify/CrossValidation.java
+++ app/src/main/java/classify/prove/doddo/libsvm_classify/CrossValidation.java
@@ -5,9 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import libsvm.svm;
-import libsvm.svm_model;
-import libsvm.svm_parameter;
 import libsvm.svm_problem;
 
 /**
@@ -17,7 +14,7 @@ public abstract class CrossValidation {
 
     public static double[][]   results;
     public static double[]     percentages;
-    public static int[] correct;
+    public static int[] exactResults;
     // TODO the user must not enter invalid values for those six variables
     public static int cStart, cEnd, cStep;
     public static int gStart, gEnd, gStep;
@@ -28,18 +25,12 @@ public abstract class CrossValidation {
     public static int[] log_C_coef;
     public static int[] log_Gamma_coef;
     public static String[] parameters;
+
     static FileOutputStream fileOutputStream;
     static BufferedOutputStream bufferedOutputStream;
 
-    static DefaultSVMParameter defaultSVMParameter;
-    static svm_parameter svmParameter;
-    static svm_model svmModel;
 
     public static void cross_validate(svm_problem svmProblem) {
-
-        defaultSVMParameter = new DefaultSVMParameter(LoadFeatureFile.f);
-        svmParameter = defaultSVMParameter.svmParameter;
-        svmModel = svm.svm_train(svmProblem, svmParameter);
 
         // Number of step = [(End - Start) / Step] + 1
         // Valid only if the Step is adequate for starting at Start and arriving exactly at End
@@ -47,18 +38,20 @@ public abstract class CrossValidation {
         cLength = ((cEnd - cStart) / cStep) +1;
         gLength = ((gEnd - gStart) / gStep) +1;
 
+        // Containers for the desired coefficients
         log_C_coef = new int[cLength];
         log_Gamma_coef = new int[gLength];
-
-        results = new double[cLength * gLength][LoadFeatureFile.l];
-        correct = new int[LoadFeatureFile.l];
-        percentages = new double[results.length];
 
         initCoefficientsArrays();
 
         // Array that stores each line of the file to be written
         parameters = new String[log_C_coef.length * log_Gamma_coef.length];
 
+        // M x N matrix that stores l results (l == rows of the model == labels) for each
+        // combination of C and Gamma
+        results = new double[cLength * gLength][LoadFeatureFile.l];
+
+        // TODO: replace numCores with actual cores on the device
         int numCores = 8;
 
         try {
@@ -72,24 +65,7 @@ public abstract class CrossValidation {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-/*
-        // Iterate the loop for every log2C and log2Gamma that the user wants to try
-        for(int i = 0; i < cLength; i++)
-        {
-            // Changing the parameter affects svmModel too
-            svmParameter.C = Math.pow(2, log_C_coef[i]);
-            for (int j = 0; j < gLength; j++)
-            {
-                svmParameter.gamma = Math.pow(2, log_Gamma_coef[j]);
-                // Do the cross validation and store the results
-                // TODO: replace 5 with the user selected number of folds
-                svm.svm_cross_validation(svmProblem, svmParameter, 2, results[gLength * i + j]);
-                // Store the parameters in the correct order of cross validation
-                parameters[gLength * i + j] =
-                        "log2c=" + log_C_coef[i] + ", log2g=" + log_Gamma_coef[j] + ", ";
-            }
-        }
-*/
+
         calculatePercentages(svmProblem);
         writeResultsToFile();
     }
@@ -130,6 +106,9 @@ public abstract class CrossValidation {
 
     private static void calculatePercentages(svm_problem svmProblem)
     {
+        exactResults = new int[results.length];
+        percentages = new double[results.length];
+
         // For every combination of C and G
         for(int i = 0; i < cLength * gLength; i++)
         {
@@ -137,9 +116,9 @@ public abstract class CrossValidation {
             for(int j = 0; j < LoadFeatureFile.l; j++)
             {
                 if(results[i][j] == svmProblem.y[j])
-                    correct[i]++;
+                    exactResults[i]++;
             }
-            percentages[i] = (double) correct[i] / LoadFeatureFile.l;
+            percentages[i] = (double) exactResults[i] / LoadFeatureFile.l;
             parameters[i] += "rate = " + (percentages[i] * 100);
         }
     }
